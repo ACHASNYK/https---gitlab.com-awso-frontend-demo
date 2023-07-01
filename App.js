e8331843518163addc5f263609620e24f6c2b20a
A.Chasnyk <a.chasnyk@gmail.com>
2023-07-01 08:43:15 +0300

    auth google

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import * as WebBrowser from "expo-web-browser";
 import * as Google from 'expo-auth-session/providers/google';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,9 +9,43 @@ WebBrowser.maybeCompleteAuthSession();
 // ios 643417147366-0k4034693h56h9bnlm4b2m6jtq5nr2bv.apps.googleusercontent.com
 export default function App() {
   const [userInfo, setUserInfo] = React.useState(null);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId: "643417147366-pifqmn3462kv5tsaggijnghokdk198vv.apps.googleusercontent.com",
+    iosClientId: "643417147366-0k4034693h56h9bnlm4b2m6jtq5nr2bv.apps.googleusercontent.com"
+  })
+
+  React.useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response])
+  async function handleSignInWithGoogle() {
+    const user = await AsyncStorage.getItem("@user");
+    if(!user) {
+      if(response?.type==="success") {
+        await getUserInfo(response.authentication.accessToken);
+      }
+
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headrs: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {console.log("an error:", error)}
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>{JSON.stringify(userInfo)}</Text>
+       <Button title="Sign in with Google" onPress={promptAsync} /> 
       <StatusBar style="auto" />
     </View>
   );
